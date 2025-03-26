8390e67bd4e63ab6c7cc66578820dcbbad0e7254
Blal <blalkhan94@gmail.com>
2025-03-26 18:49:08 +0000

    feat: added fall back image for the Heading component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -11,6 +11,11 @@ export const Header = () => {
           srcSet={BrightHRLogoMobile}
         />
         <source media="(min-width: 769px)" srcSet={BrightHRLogoDesk} />
+        <img
+          src={BrightHRLogoDesk} // Fallback image (desktop)
+          alt="bright-hr-logo"
+          data-testid="bright-hr-logo"
+        />
       </picture>
 
       <p className="telephone-number">0800 470 2432</p>
