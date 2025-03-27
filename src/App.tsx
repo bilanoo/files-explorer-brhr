630f0994d4903f27bb53c8c339c91050835d6519
Blal <blalkhan94@gmail.com>
2025-03-27 12:32:45 +0000

    feat: implemented basic filter and test case pass

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,26 @@ import "./App.css";
 import { Header } from "./components/Header/Header";
 
 function App() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <div className="main">
+        <form>
+          <div className="filter-container">
+            <label htmlFor="filter">Filter</label>
+            <input
+              aria-label="filter"
+              type="text"
+              id="filter-field"
+              name="filter"
+              placeholder="Find file..."
+              className="filter-field"
+            />
+          </div>
+        </form>
+      </div>
+    </>
+  );
 }
 
 export default App;
