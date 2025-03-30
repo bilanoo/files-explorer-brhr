ff79be36cb2ea4933b01a9940f82c1584d11278b
Blal <blalkhan94@gmail.com>
2025-03-30 02:51:30 +0100

    feat: added styling and the additional data for each card

--- src/components/DocumentCard/DocumentCard.tsx
+++ src/components/DocumentCard/DocumentCard.tsx
@@ -1,8 +1,16 @@
 export const DocumentCard = () => {
   return (
     <div className="doc-container">
+      <div className="image-container">
+        <img
+          src={"../../../public/assets/csv-file-type.svg"}
+          alt="file-type-image"
+        />
+      </div>
       <div className="content-container">
         <span>Employee Handbook</span>
+        <span className="fnt-w400">date</span>
+        <span className="fnt-w400">csv</span>
       </div>
     </div>
   );
