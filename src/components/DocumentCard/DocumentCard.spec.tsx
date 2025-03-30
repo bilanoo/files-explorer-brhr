e3daa9fe506d93699042e71755f6801b294d1076
Blal <blalkhan94@gmail.com>
2025-03-30 02:54:54 +0100

    test: updated test case where values are provided through props

--- src/components/DocumentCard/DocumentCard.spec.tsx
+++ src/components/DocumentCard/DocumentCard.spec.tsx
@@ -3,10 +3,17 @@ import { DocumentCard } from "./DocumentCard";
 
 describe("DocumentCard", () => {
   it("should display the file card with all the information", () => {
-    render(<DocumentCard />);
+    render(
+      <DocumentCard
+        image="dummy"
+        title="Handbook"
+        type="pdf"
+        date="20-03-2025"
+      />
+    );
 
     expect(screen.getByText("Employee Handbook")).toBeInTheDocument();
-    expect(screen.getByText("date")).toBeInTheDocument();
-    expect(screen.getByText("csv")).toBeInTheDocument();
+    expect(screen.getByText("20-03-2025")).toBeInTheDocument();
+    expect(screen.getByText("pdf")).toBeInTheDocument();
   });
 });
