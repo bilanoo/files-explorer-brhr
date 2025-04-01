a39933a60f079817b251e628240a96c77a5f2369
Blal <blalkhan94@gmail.com>
2025-04-01 13:20:20 +0100

    feat: added heading for the files section

--- src/components/ViewAllFiles/ViewAllFiles.tsx
+++ src/components/ViewAllFiles/ViewAllFiles.tsx
@@ -1,9 +1,11 @@
 import { DocumentCard } from "../DocumentCard/DocumentCard";
 import { IViewAllFiles } from "./ViewAllFiles.types";
+import "./ViewAllFiles.css";
 
 export const ViewAllFiles = ({ data }: IViewAllFiles) => {
   return (
     <div>
+      <h3>Files</h3>
       {data.map((documentDetails) => {
         return (
           <DocumentCard
