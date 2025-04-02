fc96fb04ce5ab8b79efbb61dfe3262a77388d86b
Blal <blalkhan94@gmail.com>
2025-04-02 18:15:21 +0100

    bug: fied unique key prop console error

--- src/components/ViewAllFiles/ViewAllFiles.tsx
+++ src/components/ViewAllFiles/ViewAllFiles.tsx
@@ -7,30 +7,26 @@ export const ViewAllFiles = ({ data }: IViewAllFiles) => {
   return (
     <div>
       <h3>Files</h3>
-      {data.map((documentDetails) => {
-        return (
-          <>
-            {documentDetails.type === "folder" ? (
-              <FolderCard
-                image={documentDetails.image}
-                title={documentDetails.name}
-                type={documentDetails.type}
-                date={documentDetails.added || ""}
-                files={documentDetails.files ?? null}
-                key={documentDetails.name}
-              />
-            ) : (
-              <DocumentCard
-                image={documentDetails.image}
-                title={documentDetails.name}
-                type={documentDetails.type}
-                date={documentDetails.added || ""}
-                key={documentDetails.name}
-              />
-            )}
-          </>
-        );
-      })}
+      {data.map((documentDetails) =>
+        documentDetails.type === "folder" ? (
+          <FolderCard
+            key={documentDetails.name}
+            image={documentDetails.image}
+            title={documentDetails.name}
+            type={documentDetails.type}
+            date={documentDetails.added || ""}
+            files={documentDetails.files ?? null}
+          />
+        ) : (
+          <DocumentCard
+            key={documentDetails.name}
+            image={documentDetails.image}
+            title={documentDetails.name}
+            type={documentDetails.type}
+            date={documentDetails.added || ""}
+          />
+        )
+      )}
     </div>
   );
 };
