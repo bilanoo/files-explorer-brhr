8d72c2fe36bc819b0ecbe0f3711fa4aec51f846c
Blal <blalkhan94@gmail.com>
2025-04-02 16:24:20 +0100

    test: failing test case for the  filtering component

---
+++ src/components/Filter/Filter.test.tsx
@@ -0,0 +1,11 @@
+import { render, screen } from "@testing-library/react";
+import { Filter } from "./Filter";
+
+describe("Filter", () => {
+  it("should test the search input field", () => {
+    render(<Filter />);
+    const input = screen.getByLabelText("filter");
+    fireEvent.change(input, { target: { value: "Dolly" } });
+    expect(input.value).toBe("Dolly");
+  });
+});
