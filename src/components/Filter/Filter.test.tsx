51d7837ab651223c2e0dedef70a0afe4c814a10c
Blal <blalkhan94@gmail.com>
2025-04-02 17:32:38 +0100

    test: added failing test case to also test the clear button

--- src/components/Filter/Filter.test.tsx
+++ src/components/Filter/Filter.test.tsx
@@ -3,9 +3,37 @@ import { Filter } from "./Filter";
 
 describe("Filter", () => {
   it("should test the search input field", () => {
-    render(<Filter />);
+    let filter = false;
+    const mockSetIsFiltering = () => {
+      filter = !filter;
+    };
+    render(
+      <Filter
+        data={[]}
+        setFilteredData={() => jest.mock}
+        setIsFiltering={mockSetIsFiltering}
+      />
+    );
     const input = screen.getByLabelText("filter");
     fireEvent.change(input, { target: { value: "Dolly" } });
     expect(input.value).toBe("Dolly");
+  });
+  it("should test if the input field clears after clicking the clear icon button", () => {
+    let filter = false;
+    const mockSetIsFiltering = () => {
+      filter = !filter;
+    };
+    render(
+      <Filter
+        data={[]}
+        setFilteredData={() => jest.mock}
+        setIsFiltering={mockSetIsFiltering}
+      />
+    );
+    const input = screen.getByLabelText("filter");
+    fireEvent.change(input, { target: { value: "Dolly" } });
+    expect(input.value).toBe("Dolly");
+    fireEvent.click(screen.getAllByRole("button")[1]);
+    expect(input.value).not.toBe("Dolly");
   });
 });
